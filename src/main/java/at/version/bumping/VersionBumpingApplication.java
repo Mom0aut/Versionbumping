d2f7c16ba0a855477b39fd769866743f9537514c
Your Name <reinhart19_91@hotmail.com>
2023-04-22 01:05:14 +0200

    init

---
+++ src/main/java/at/version/bumping/VersionBumpingApplication.java
@@ -0,0 +1,13 @@
+package at.version.bumping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VersionBumpingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VersionBumpingApplication.class, args);
+	}
+
+}
